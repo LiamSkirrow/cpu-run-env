59c92529fcfd909cbb8fb1d42f44995651d1bd3a
Liam Skirrow <skirrowliam@gmail.com>
2025-10-15 23:52:29 +0100

    Read in ELF file to elf tools ELF class

--- scripts/readelf.py
+++ scripts/readelf.py
@@ -2,4 +2,19 @@
 # to determine the start and end labels for the user asm (WRAPSTART/WRAPEND)
 # and splice out the corresponding chunk of machine code
 
-import readelf
+from elftools.elf.elffile import ELFFile
+from argparse import ArgumentParser
+
+# handle input args
+parser = ArgumentParser()
+parser.add_argument("-e", "--exename", required=True, type=str, help="Give the name of the executable, used to lookup the relevant exe")
+
+args = parser.parse_args()
+
+exe_name = args.exename
+exe_path = 'gen-output/' + exe_name
+print(exe_path)
+
+# elfFile = ELFFile(open(exe_path, 'r', encoding='latin-1'))
+elfFile = ELFFile(open(exe_path, 'rb'))
+print('Success?...')
