5937a5a99f846df0b9cb1364ca87ba8f1d500de7
Liam Skirrow <skirrowliam@gmail.com>
2025-11-01 14:26:04 +0000

    Add new test

---
+++ tests-asm/test.s
@@ -0,0 +1,20 @@
+#li a4,31
+addi x14, x0, 5
+EBREAK
+#li a4,255
+#li a4,254
+#li a4,11
+#li a4,12
+#li a4,13
+#li a4,14
+#li a4,15
+#li a4,16
+#li a4,17
+#li a4,18
+#li a4,19
+#li a4,20
+#li a4,21
+#li a4,22
+#li a4,23
+#li a4,24
+#li a4,25
